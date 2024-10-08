7f00e8000181af4593e21cb4c945dbb0d0880276
Evheniy Nahola <evgeniy.nagola@gmail.com>
2024-10-08 12:36:33 +0300

    anim fix

--- cypex/js/common.js
+++ cypex/js/common.js
@@ -1,40 +1,62 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
-    gsap.registerPlugin(ScrollTrigger);
-    gsap.registerPlugin(CSSRulePlugin)
 
-    // document.querySelectorAll('.productsTab__nav__item').forEach(tab => {
-    //     tab.addEventListener('click', function(event) {
-    //         event.preventDefault();
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(CSSRulePlugin);
+
+  const homePage = document.querySelector('#homePage');
+
+  if (homePage) {
     
-    //         document.querySelectorAll('.productsTab__nav__item').forEach(item => item.classList.remove('active'));
-    //         document.querySelectorAll('.productsTab__item').forEach(content => content.classList.remove('active'));
-    
-    //         this.classList.add('active');
-    //         const targetTab = this.getAttribute('data-tab');
-    //         document.getElementById(targetTab).classList.add('active');
-    //     });
-    // });
+    //Text block
+  function wrapTextWithSpans(element) {
+    const text = element.textContent;
+
+    const wrappedText = text.split(' ').map(word => {
+      return `<span>${word}</span>`;
+    }).join(' ');
+
+    element.innerHTML = wrappedText;
+  }
+
+  const textElement = document.querySelector('.keyPoints__description p');
+
+  if (textElement) {
+    wrapTextWithSpans(textElement);
+
+    gsap.from(".keyPoints__description p span", {
+      scrollTrigger: {
+        trigger: ".keyPoints__description",
+        start: "top 90%",
+        end: "bottom 60%",
+        scrub: true,
+      },
+      opacity: 0.2,
+      y: 20,
+      duration: 1.5,
+      stagger: 0.04,
+      ease: "power2.out"
+    });
+  }
 
     document.querySelectorAll('.productsTab__nav__item').forEach(tab => {
-      tab.addEventListener('click', function(event) {
+      tab.addEventListener('click', function (event) {
         event.preventDefault();
-    
+
         const activeContent = document.querySelector('.productsTab__item.active');
         const newTab = this.getAttribute('data-tab');
         const newContent = document.getElementById(newTab);
-    
+
         if (activeContent === newContent) {
           return;
         }
-    
+
         gsap.to(activeContent, {
           opacity: 0,
           y: 50,
           duration: 0.5,
           onComplete: () => {
             activeContent.classList.remove('active');
-    
+
             newContent.classList.add('active');
             gsap.fromTo(newContent, {
               opacity: 0,
@@ -52,33 +74,15 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     });
 
-    const hamburgerMenu = document.querySelector('.hamburger');
-    const mainMenu = document.querySelector('.mainMenu__wrapper');
-
-    hamburgerMenu.addEventListener('click', function(){
-        this.classList.toggle('is-active');
-        mainMenu.classList.toggle('active');
-        document.querySelector('body').classList.toggle('menuOpen');
-    });
-
-    const mainForm = document.querySelector('.mainForm');
-    const thanksWindow = document.querySelector('.thanksWindow');
-
-    document.addEventListener('submit', function(event) {
-        event.preventDefault();
-        mainForm.classList.add('disabled');
-        thanksWindow.classList.add('active');
-    });
-
     document.querySelectorAll('.productsTab__item').forEach(item => {
       const checkbox = item.querySelector('.toggleCheckbox');
-    
-      checkbox.addEventListener('change', function() {
+
+      checkbox.addEventListener('change', function () {
         const changeableElements = item.querySelectorAll('.changeable');
-        
+
         const visibleElements = [...changeableElements].filter(el => !el.classList.contains('hidden'));
         const hiddenElements = [...changeableElements].filter(el => el.classList.contains('hidden'));
-    
+
         gsap.to(visibleElements, {
           opacity: 0,
           x: -100,
@@ -86,7 +90,7 @@ document.addEventListener("DOMContentLoaded", () => {
           ease: "power2.out",
           onComplete: () => {
             visibleElements.forEach(el => el.classList.add('hidden'));
-    
+
             gsap.fromTo(hiddenElements, {
               opacity: 0,
               x: 100
@@ -104,15 +108,132 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     });
 
-      //Key items
-      const infoItems = document.querySelectorAll('.keyPoints__anim');
+    //Key items
+    const infoItems = document.querySelectorAll('.keyPoints__anim');
 
-      gsap.from(infoItems, {
+    gsap.from(infoItems, {
+      scrollTrigger: {
+        trigger: '.keyPoints',
+        start: "top 70%",
+        end: "bottom 40%",
+        toggleActions: "play none none none",
+      },
+      opacity: 0,
+      y: 200,
+      scale: 1.5,
+      filter: 'blur(10px)',
+      duration: 1.1,
+      stagger: 0.4,
+      ease: "power2.out",
+    });
+
+    //Products items
+    const productsItems = document.querySelectorAll('.products__anim');
+
+    gsap.from(productsItems, {
+      scrollTrigger: {
+        trigger: '.products',
+        start: "top 90%",
+        end: "bottom 40%",
+        toggleActions: "play none none none",
+      },
+      opacity: 0,
+      y: 200,
+      scale: 1.5,
+      filter: 'blur(10px)',
+      duration: 1.1,
+      stagger: 0.4,
+      ease: "power2.out",
+    });
+
+    //strength items
+    const strengthItems = document.querySelectorAll('.mainStrengths__anim');
+
+    gsap.from(strengthItems, {
+      scrollTrigger: {
+        trigger: '.mainStrengths__wrapper',
+        start: "top 60%",
+        end: "bottom 40%",
+        toggleActions: "play none none none",
+      },
+      opacity: 0,
+      y: 200,
+      scale: 1.5,
+      filter: 'blur(10px)',
+      duration: 1.1,
+      stagger: 0.4,
+      ease: "power2.out",
+    });
+
+    //callback items
+    const callbackItems = document.querySelectorAll('.callback__anim');
+
+    gsap.from(callbackItems, {
+      scrollTrigger: {
+        trigger: '.callback',
+        start: "top 70%",
+        end: "bottom 30%",
+        toggleActions: "play none none none",
+      },
+      opacity: 0,
+      y: 200,
+      scale: 1.5,
+      filter: 'blur(10px)',
+      duration: 1.1,
+      stagger: 0.4,
+      ease: "power2.out",
+    });
+
+  }
+
+  const hamburgerMenu = document.querySelector('.hamburger');
+  const mainMenu = document.querySelector('.mainMenu__wrapper');
+
+  hamburgerMenu.addEventListener('click', function () {
+    this.classList.toggle('is-active');
+    mainMenu.classList.toggle('active');
+    document.querySelector('body').classList.toggle('menuOpen');
+  });
+
+  const toggleForm = document.querySelector('.toggleForm');
+  const toggledFormItem = document.querySelector('.ourJourney__callback');
+
+  if (toggleForm) {
+    toggleForm.addEventListener('click', () => {
+      toggledFormItem.classList.remove('hideable');
+    })
+  }
+
+  const faqItems = document.querySelectorAll('.faq__item');
+
+    if (faqItems) {
+      faqItems.forEach(item => {
+        const control = item.querySelector('.faq__item__control');
+
+        control.addEventListener('click', () => {
+          const isActive = item.classList.contains('active');
+
+          faqItems.forEach(i => i.classList.remove('active'));
+
+          if (!isActive) {
+            item.classList.add('active');
+          }
+        });
+      });
+    }
+
+    const cases = document.querySelector('#cases');
+
+    if (cases) {
+      const casesItems = document.querySelectorAll('.cases__item');
+
+      gsap.from(casesItems, {
         scrollTrigger: {
-          trigger: '.keyPoints',
-          start: "top 70%",
-          end: "bottom 40%",
+          trigger: '.cases__wrapper',
+          start: "top 90%",
+          end: "bottom 80%",
           toggleActions: "play none none none",
+          scrub: true,
         },
         opacity: 0,
         y: 200,
@@ -120,93 +241,59 @@ document.addEventListener("DOMContentLoaded", () => {
         filter: 'blur(10px)',
         duration: 1.1,
         stagger: 0.4,
-        ease: "power2.out",
+        ease: "power2.out"
       });
+    }
+
+    const caseStudies = document.querySelector('#caseStudies');
+
+    if (caseStudies) {
 
       //Text block
       function wrapTextWithSpans(element) {
         const text = element.textContent;
-        const wrappedText = text.split('').map(char => {
-          // Обгортання кожного символу в <span>
-          return char === ' ' ? `<span style="white-space: pre;"> </span>` : `<span>${char}</span>`;
-        }).join('');
-        
+
+        const wrappedText = text.split(' ').map(word => {
+          return `<span>${word}</span>`;
+        }).join(' ');
+
         element.innerHTML = wrappedText;
       }
-      
+
       const textElement = document.querySelector('.keyPoints__description p');
-      wrapTextWithSpans(textElement);
-      
-      gsap.from(".keyPoints__description p span", {
-        scrollTrigger: {
-          trigger: ".keyPoints__description",
-          start: "top 90%",
-          end: "bottom 60%",
-          scrub: true,
-        },
-        opacity: 0.2,
-        y: 30,
-        duration: 1.5,
-        stagger: 0.04,
-        ease: "power2.out"
+
+      if (textElement) {
+        wrapTextWithSpans(textElement);
+
+        gsap.from(".keyPoints__description p span", {
+          scrollTrigger: {
+            trigger: ".keyPoints__description",
+            start: "top 70%",
+            end: "bottom 40%",
+            scrub: true,
+          },
+          opacity: 0.2,
+          y: 20,
+          duration: 1.5,
+          stagger: 0.04,
+          ease: "power2.out"
+        });
+      }
+
+      const stepsItems = document.querySelectorAll('.caseStudies__stepsItem');
+
+      stepsItems.forEach((item) => {
+        gsap.to(item, {
+          scrollTrigger: {
+            trigger: item,
+            start: "top top",
+            end: "center 50%",
+            toggleClass: "active",
+            markers: false,
+          }
+        });
       });
 
-
-      //Products items
-      const productsItems = document.querySelectorAll('.products__anim');
-
-      gsap.from(productsItems, {
-        scrollTrigger: {
-          trigger: '.products',
-          start: "top 90%",
-          end: "bottom 40%",
-          toggleActions: "play none none none",
-        },
-        opacity: 0,
-        y: 200,
-        scale: 1.5,
-        filter: 'blur(10px)',
-        duration: 1.1,
-        stagger: 0.4,
-        ease: "power2.out",
-      });
-
-      //strength items
-      const strengthItems = document.querySelectorAll('.mainStrengths__anim');
-
-      gsap.from(strengthItems, {
-        scrollTrigger: {
-          trigger: '.mainStrengths__wrapper',
-          start: "top 60%",
-          end: "bottom 40%",
-          toggleActions: "play none none none",
-        },
-        opacity: 0,
-        y: 200,
-        scale: 1.5,
-        filter: 'blur(10px)',
-        duration: 1.1,
-        stagger: 0.4,
-        ease: "power2.out",
-      });
-
-      //callback items
-      const callbackItems = document.querySelectorAll('.callback__anim');
-
-      gsap.from(callbackItems, {
-        scrollTrigger: {
-          trigger: '.callback',
-          start: "top 70%",
-          end: "bottom 30%",
-          toggleActions: "play none none none",
-        },
-        opacity: 0,
-        y: 200,
-        scale: 1.5,
-        filter: 'blur(10px)',
-        duration: 1.1,
-        stagger: 0.4,
-        ease: "power2.out",
-      });
+    }
 
 });
